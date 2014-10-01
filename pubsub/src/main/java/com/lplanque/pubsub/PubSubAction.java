d7d5e50306179d60ed7f20fd877321d688551bd1
lplanque <lplanque@gmail.com>
2014-10-01 09:52:47 +0200

    This type is not needed yet

--- pubsub/src/main/java/com/lplanque/pubsub/PubSubAction.java
+++
@@ -1,8 +0,0 @@
-package com.lplanque.pubsub;
-
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPubSub;
-
-public interface PubSubAction {
-	void doIt(final JedisPubSub ps, final String ch, final Jedis jedis);
-}
