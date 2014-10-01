4ebe239cfbe42b584ac4ee2e49c6131a09a13551
lplanque <lplanque@gmail.com>
2014-10-01 10:01:17 +0200

    Light refactor

--- pubsub/src/main/java/com/lplanque/pubsub/PubSubs.java
+++ pubsub/src/main/java/com/lplanque/pubsub/PubSubs.java
@@ -16,7 +16,7 @@ public final class PubSubs {
 		int i = 0;
 		for(Object param: params) {
 			if(param == null) {
-				final String msg = String.format("%d parameter of method \"%s\" is null", i, meth);
+				final String msg = String.format("%d parameter for \"%s\" is null", i, meth);
 				throw new RuntimeException(msg);
 			}
 		}
@@ -31,17 +31,20 @@ public final class PubSubs {
 	}
 	
 	public static void bulkPub(final String msg, final String ch, final JedisPool... pools) {
-		assertNotNull("bulkPub", msg, ch, pools);
+		final String meth = "bulkPub(String, String, JedisPool...)";
+		assertNotNull(meth, msg, ch, pools);
 		bulk(j -> j.publish(ch, msg), pools);
 	}
 	
 	public static void bulkSub(final JedisPubSub ps, final String ch, final JedisPool... pools) {
-		assertNotNull("bulkSub", ps, ch, pools);
+		final String meth = "bulkSub(JedisPubSub, String, JedisPool...)";
+		assertNotNull(meth, ps, ch, pools);
 		bulk(j -> j.subscribe(ps, ch), pools);
 	}
 	
 	public static void ohe(final String ch, final JedisPool... pools) { // TODO refactor
-		assertNotNull("ohe", ch, pools);
+		final String meth = "ohe(String, JedisPool...)";
+		assertNotNull(meth, ch, pools);
 		bulk(j -> j.publish(ch, String.format("ohé %s", j)), pools);
 	}
 }
